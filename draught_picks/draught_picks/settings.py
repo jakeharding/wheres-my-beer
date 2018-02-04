179e056caf49098f8909d3ed968cd1742ca3515f
Jake Harding <jacobeharding@gmail.com>
2018-02-03 19:33:33 -0600

    Update requirements and DRF settings.

--- draught_picks/draught_picks/settings.py
+++ draught_picks/draught_picks/settings.py
@@ -127,6 +127,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
+}
+
 try:
     from .local_settings import *
 except ImportError:
