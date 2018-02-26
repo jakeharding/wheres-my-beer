b35e66ba68733c4670d305f6935f6a8df6c61211
carol_8wybosj <catt12345>
2018-02-26 16:46:29 -0600

    Added endpoints

--- draught_picks/beers/tests.py
+++ draught_picks/beers/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+"""
+tests.py - (C) Copyright - 2018
+This software is copyrighted to contributors listed in CONTRIBUTIONS.md.
 
-# Create your tests here.
+SPDX-License-Identifier: SEE LICENSE.txt
+
+Author(s) of this file:
+  carolynwichers
+
+Test user endpoints.
+"""
+
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+
+class TestBeers(APITestCase):
+
+    def setUp(self):
+        self.beer = get_user_model().objects.first()
+
--- draught_picks/beers/views.py
+++ draught_picks/beers/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+"""
+views.py - (C) Copyright - 2018
+This software is copyrighted to contributors listed in CONTRIBUTIONS.md.
 
-# Create your views here.
+SPDX-License-Identifier: SEE LICENSE.txt
+
+Author(s) of this file:
+  carolynwichers
+
+Expose user models through a REST API.
+"""
+
+from rest_framework.serializers import ModelSerializer
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, UpdateModelMixin, RetrieveModelMixin
+from rest_framework.permissions import AllowAny
+
+from .models import Beer, BeerRating, RecentBeer
+
+
+class BeerSerializer(ModelSerializer):
+    class Meta:
+        model = Beer
+        fields = ('uuid', 'name', 'description', 'abv', 'ibu', 'api_id', 'name_of_api', 'created_at',)
+
+
+class BeerSet(CreateModelMixin, ListModelMixin, UpdateModelMixin, RetrieveModelMixin, GenericViewSet):
+    serializer_class = BeerSerializer
+    queryset = Beer.objects.all()
+    lookup_field = 'uuid'
+    permission_classes = (AllowAny, )
+
+
+class BeerRatingSerializer(ModelSerializer):
+    class Meta:
+        model = BeerRating
+        fields = ('uuid', 'user', 'beer', 'rating', 'description', 'created_at',)
+
+
+class BeerRatingSet(CreateModelMixin, ListModelMixin, UpdateModelMixin, RetrieveModelMixin, GenericViewSet):
+    serializer_class = BeerRatingSerializer
+    queryset = BeerRating.objects.all()
+    lookup_field = 'uuid'
+    permission_classes = (AllowAny, )
+
+
+class RecentBeerSerializer(ModelSerializer):
+    class Meta:
+        model = RecentBeer
+        fields = ('uuid', 'user', 'beer', 'created_at',)
+
+
+class RecentBeerSet(CreateModelMixin, ListModelMixin, UpdateModelMixin, RetrieveModelMixin, GenericViewSet):
+    serializer_class = RecentBeerSerializer
+    queryset = RecentBeer.objects.all()
+    lookup_field = 'uuid'
+    permission_classes = (AllowAny, )
