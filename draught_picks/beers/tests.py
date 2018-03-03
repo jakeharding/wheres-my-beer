52f9bf56cf7085d9a2d01230ace968bf3c7a6239
Jake Harding <jacobeharding@gmail.com>
2018-03-03 14:29:09 -0600

    Fix tests.

--- draught_picks/beers/tests.py
+++ draught_picks/beers/tests.py
@@ -11,15 +11,18 @@ Test user endpoints.
 """
 
 from beers.models import Beer
+from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 
 class TestBeers(APITestCase):
-    fixtures = ['beers/fixtures/beers.json']
+    fixtures = ['beers/fixtures/beers.json','users/fixtures/users.json']
 
     def setUp(self):
         self.beer = Beer.objects.first()
+        self.user = get_user_model().objects.first()
+        self.client.force_authenticate(user=self.user)
 
     def test_retrieve(self):
         r = self.client.get('/api/dev/beers/%s' % self.beer.uuid)
