605e863cd24dfd9334946a285863aa0383309b6d
Jake Harding <jacobeharding@gmail.com>
2018-03-03 13:55:49 -0600

    Beer with rating serializer for beer viewset.

--- draught_picks/beers/views.py
+++ draught_picks/beers/views.py
@@ -9,7 +9,7 @@ Author(s) of this file:
 
 Expose user models through a REST API.
 """
-from rest_framework.serializers import ModelSerializer, UUIDField, SlugRelatedField
+from rest_framework.serializers import ModelSerializer, UUIDField, SlugRelatedField, SerializerMethodField
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import CreateModelMixin, ListModelMixin, UpdateModelMixin, RetrieveModelMixin
 from rest_framework.filters import SearchFilter
@@ -27,6 +27,30 @@ class BeerSerializer(ModelSerializer):
         exclude = ('id', )
 
 
+class BeerRatingSerializer(ModelSerializer):
+    user = SlugRelatedField(slug_field='uuid', queryset=DraughtPicksUser.objects.all())
+    beer = SlugRelatedField(slug_field='uuid', queryset=Beer.objects.all())
+    uuid = UUIDField(read_only=True)
+
+    class Meta:
+        model = BeerRating
+        fields = ('uuid', 'user', 'beer', 'rating', 'description', 'created_at',)
+
+
+class BeerWithRatingSerializer(BeerSerializer):
+    """
+    Serializer to include the rating by the given user.
+    """
+    rating = SerializerMethodField()
+
+    def __init__(self, data, user=None, **kwargs):
+        super().__init__(data, **kwargs)
+        self.user = user
+
+    def get_rating(self, obj):
+        return BeerRatingSerializer(BeerRating.objects.filter(beer=obj, user=self.user), many=True).data
+
+
 class BeerSet(CreateModelMixin, ListModelMixin, UpdateModelMixin, RetrieveModelMixin, GenericViewSet):
     serializer_class = BeerSerializer
     queryset = Beer.objects.all()
@@ -35,16 +59,13 @@ class BeerSet(CreateModelMixin, ListModelMixin, UpdateModelMixin, RetrieveModelM
     filter_backends = (SearchFilter, )
 
 
-class BeerRatingSerializer(ModelSerializer):
-    class Meta:
-        model = BeerRating
-        fields = ('uuid', 'user', 'beer', 'rating', 'description', 'created_at',)
-
-
 class BeerRatingSet(CreateModelMixin, ListModelMixin, UpdateModelMixin, RetrieveModelMixin, GenericViewSet):
     serializer_class = BeerRatingSerializer
     queryset = BeerRating.objects.all()
     lookup_field = 'uuid'
+
+    def get_queryset(self):
+        return BeerRating.objects.filter(user=self.request.user)
 
 
 class RecentBeerSerializer(ModelSerializer):
@@ -65,19 +86,20 @@ class RecentBeerSet(CreateModelMixin, ListModelMixin, GenericViewSet):
     def get_queryset(self):
         return RecentBeer.objects.filter(user=self.request.user)
 
-    def list(self, *arg, **kwargs):
+    def list(self, request, *arg, **kwargs):
         """
         Override to serialize the beers and not the through table.
+        :param request:
         :param arg:
         :param kwargs:
         :return:
         """
-        qs = self.request.user.recent_beers.all()
+        qs = self.request.user.recent_beers.order_by('created_at')
         page = self.paginate_queryset(qs)
 
         if page is not None:
-            serializer = BeerSerializer(page, many=True)
+            serializer = BeerWithRatingSerializer(page, user=request.user, many=True)
             return self.get_paginated_response(serializer.data)
 
-        serializer = BeerSerializer(qs, many=True)
+        serializer = BeerWithRatingSerializer(qs, user=request.user, many=True)
         return Response(serializer.data)
